6158c1e144b8ae92a4075aaf9c218dcfe2dd7bcf
LALIT JAGTAP <jagtap_lalit@yahoo.com>
2018-07-01 16:08:18 -0500

    Complete but missing saving of people names and images.

---
+++ MetWhenWhere/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  MetWhenWhere
+//
+//  Created by LALIT JAGTAP on 6/30/18.
+//  Copyright © 2018 LALIT JAGTAP. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name : String
+    var imageName : String
+    
+    init(name: String, imageName: String) {
+        self.name = name
+        self.imageName = imageName
+    }
+    
+}
---
+++ MetWhenWhere/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  MetWhenWhere
+//
+//  Created by LALIT JAGTAP on 6/30/18.
+//  Copyright © 2018 LALIT JAGTAP. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
